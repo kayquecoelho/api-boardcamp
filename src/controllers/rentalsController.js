c48d3667d036e1550f1ddd7d51549c1227662bb9
Kayque <lkcoelho76@gmail.com>
2022-03-03 09:34:12 -0300

    feat: add feature to create and read rentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,6 +1,58 @@
 import connection from "../database/database.js";
 import dayjs from "dayjs";
 
+export async function getRentals (req, res) {
+  let { customerId, gameId } = req.query;
+
+  if (!customerId) {
+    customerId = "";
+  }
+  if (gameId) {
+    gameId = "";
+  }
+
+  try {
+    const { rows: result} = await connection.query(`
+      SELECT 
+        r.*, g."categoryId", 
+        g.name "gameName", 
+        cas.name "categoryName", 
+        cus.name "customerName"
+      FROM rentals r
+        JOIN games g ON r."gameId"=g.id
+        JOIN categories cas ON cas.id=g."categoryId"
+        JOIN customers cus ON cus.id=r."customerId"
+    `);
+   
+    const rentals = result.map(r => {
+      const formatRental = {
+        ...r,
+        customer: { 
+          id: r.customerId,
+          name: r.customerName
+        },
+        game: {
+          id: r.gameId,
+          name: r.gameName,
+          categoryId: r.categoryId,
+          categoryName: r.categoryName
+        }
+      }
+
+      delete formatRental.categoryName
+      delete formatRental.categoryId
+      delete formatRental.customerName
+      delete formatRental.gameName
+
+      return formatRental;
+    })
+    res.send(rentals)
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+
 export async function addRental(req, res) {
   const { gameId, customerId, daysRented } = req.body;
   const { priceOfGame } = res.locals;
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { addRental } from "../controllers/rentalsController.js";
+import { addRental, getRentals } from "../controllers/rentalsController.js";
 import { validateRentalSchema } from "../middlewares/validateRentalSchema.js";
 
 const rentalsRouter = Router();
 
-//rentalsRouter.get("/rentals", getGames);
+rentalsRouter.get("/rentals", getRentals);
 rentalsRouter.post("/rentals", validateRentalSchema, addRental);
 
 export default rentalsRouter;
