dc85d7db7d876c9232d445662545c0f7ed7f4636
Kayque <lkcoelho76@gmail.com>
2022-03-10 15:46:02 -0300

    fix: change comparison to null value and change scope of gameIdExists

---
+++ src/middlewares/validateRentalSchemaData.js
@@ -0,0 +1,37 @@
+import connection from "../database/database.js";
+
+export async function validateRentalSchemaData(req, res, next) {
+  try {
+    const gameIdExists = await connection.query(`
+      SELECT id, "stockTotal", "pricePerDay" FROM games 
+        WHERE id=$1
+    `, [req.body.gameId]);
+
+    if (gameIdExists.rowCount === 0) {
+      return res.sendStatus(400);
+    }
+
+    const customerIdExists = await connection.query(`
+      SELECT id FROM customers
+        WHERE id=$1
+    `, [req.body.customerId]);
+    
+    if (customerIdExists.rowCount === 0) {
+      return res.sendStatus(400);
+    }
+
+    const amountOfRentedGames = await connection.query(`
+      SELECT id FROM rentals WHERE "gameId"=$1 AND "returnDate" IS NULL
+    `, [req.body.gameId]);
+
+    if (amountOfRentedGames.rowCount >= gameIdExists.rows[0].stockTotal) {
+      return res.sendStatus(400);
+    }
+    
+    res.locals.priceOfGame = gameIdExists.rows[0].pricePerDay;
+  } catch (error) {
+    res.status(500).send(error);
+  }
+
+  next();
+}
