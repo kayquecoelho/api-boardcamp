367cdaf675380e2373f9f78e3a03afa033f2e0b9
Paulo Piotto <piotto@pop-os.localdomain>
2022-03-10 15:30:13 -0300

    preparando revisao

--- src/schemas/rentalSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-const rentalSchema = joi.object({
-  customerId: joi.number().integer().positive().required(),
-  gameId: joi.number().integer().positive().required(),
-  daysRented: joi.number().integer().positive().required()
-});
-
-export default rentalSchema;
